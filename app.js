a9f13f1209d0836fa3edb673ea53682aa8f46604
rachelharu <rachel.haru9@gmail.com>
2021-10-22 14:33:22 -0700

    added get request to app.js. moved images and css files to public folder for display on public server

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+res.sendFile(__dirname + "/signup.html")
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
