19416b183fa6a102f2be6362a0cf438e07a609a1
rachelharu <rachel.haru9@gmail.com>
2021-10-22 14:57:18 -0700

    added post request to app.js

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// allows you to use files stored on your computer to display on public page
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) => {
 res.sendFile(__dirname + "/signup.html")
+});
+
+app.post("/", (req, res) => {
+  let firstName = req.body.fname;
+  let lastName =  req.body.lname;
+  const email =   req.body.email;
+
+  console.log(firstName, lastName, email);
 });
 
 app.listen(3000, () => {
